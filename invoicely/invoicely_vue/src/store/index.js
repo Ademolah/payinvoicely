6091a4dd3d9019d06c3cbe07ed536d727f3c6d13
Charles <adeakinyemi129@gmail.com>
2024-12-03 11:44:06 +0100

    mod app.vue

--- invoicely/invoicely_vue/src/store/index.js
+++ invoicely/invoicely_vue/src/store/index.js
@@ -23,6 +23,10 @@ export default createStore({
     setToken(state, token) {
       state.token = token
       state.isAuthenticated = true
+    },
+    removeToken(state){
+      state.token = ''
+      state.isAuthenticated = false
     }
   },
   actions: {
